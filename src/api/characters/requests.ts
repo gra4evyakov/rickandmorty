0890ddb93cee35ad1879f7fefb634711f2b7b96f
gra4evyakov <gra4evyakov@yandex.ru>
2023-05-31 12:42:57 +0300

    del hook from requests.ts

--- src/api/characters/requests.ts
+++ src/api/characters/requests.ts
@@ -1,30 +1,5 @@
-import { ref, type Ref } from 'vue'
 import { apiService } from '@/api/characters/services'
-import type { RequestParams, RequestFunction, RequestResult } from '@/api/characters/types'
-
-const useRequest = <T>(requestFunction: RequestFunction<T>): RequestResult<T> => {
-  const data: Ref<T | null> = ref(null)
-  const isLoading: Ref<boolean> = ref(false)
-  const isError: Ref<boolean> = ref(false)
-
-  const execute = async (params?: RequestParams): Promise<void> => {
-    isLoading.value = true
-    try {
-      data.value = await requestFunction(params)
-    } catch {
-      isError.value = true
-    } finally {
-      isLoading.value = false
-    }
-  }
-
-  return {
-    data,
-    isLoading,
-    isError,
-    execute
-  }
-}
+import { useRequest } from '@/hooks/useRequest'
 
 export const useGetCharacters = () => {
   return useRequest(apiService.getCharacters)
